e4e7571ebee712d2ef76193f2f3d684a484ae25a
Steven <stzheng716@gmail.com>
2023-06-12 17:25:53 -0700

    make all the test for this pass

--- linked-list.js
+++ linked-list.js
@@ -54,7 +54,7 @@ class LinkedList {
 
   /** pop(): return & remove last item. */
 
-  pop() { 
+  pop() {
     if (!this.head) {
       throw new Error("no head");
     }
@@ -65,15 +65,15 @@ class LinkedList {
       removed = this.head
       this.head = null;
       this.tail = null;
-      this.length --
+      this.length--
       return removed.val
-    } 
+    }
 
     // all other lengths apply here
     let current = this.head;
     let count = 0;
     while (current !== null) {
-      if (count === this.length - 2){
+      if (count === this.length - 2) {
         removed = current.next
         current.next = null
         this.tail = current
@@ -82,7 +82,7 @@ class LinkedList {
       return removed.val
     }
   }
-  
+
 
   /** shift(): return & remove first item. */
 
@@ -104,49 +104,58 @@ class LinkedList {
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-    if (idx > this.length || idx <= 0) throw new Error("invalid index");
+    if (idx > this.length || idx < 0) throw new Error("invalid index");
 
     let current = this.head;
-    let count = 1;
-    while (current !== null) {
-      if (count === idx) {
-        return current.val;
-      }
+    let count = 0;
+    while (current !== null && count !== idx) {
       count++;
       current = current.next;
     }
+
+    return current.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-    if (idx >= this.length || idx < 0){
+    if (idx >= this.length || idx < 0) {
       throw new Error("invalid index");
     }
+
+    if (idx === 0) this.head.val = val
+
     let current = this.head;
     let count = 0;
+    // let prev;
 
-    while(current !== null && count !== idx){
-      if (count === idx) {
+    while (current !== null) {
+      // if (idx === count - 1) {
+      //   prev = current
+      // }
+      if (idx === count)  {
         current.val = val
+        // prev.next = current
+      }
+      current = current.next
+      count++
     }
-    count++
   }
-}
   //
 
   /** insertAt(idx, val): add node w/val before idx. */
 
   insertAt(idx, val) {
-    if (idx >= this.length || idx < 0) throw new Error("invalid index");
-
     if (idx === 0) return this.unshift(val);
     if (idx === this.length) return this.push(val);
+
+    if (idx >= this.length || idx < 0) throw new Error("invalid index");
+
 
     let newNode = new Node(val);
     let current = this.head;
     let previous;
-    let count = 1;
+    let count = 0;
     while (current !== null) {
       if (count === idx - 1) {
         previous = current;
@@ -165,43 +174,66 @@ class LinkedList {
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-    if(this.head === null) return undefined;
+    if (idx >= this.length || idx < 0) {
+      throw new Error("Invalid index.");
+    }
+
+    if (idx === 0) return this.shift()
+    if (idx === this.length - 1) return this.pop()
 
     let current = this.head
     let count = 0;
     let previous;
     let nodeToBeRemoved;
-
-    while(current.next){
-      if(idx === count - 1){
+    //5, 10, 15, 20
+    while (current.next !== null && count !== idx) {
+      if (count === idx - 1) {
         previous = current
       }
 
-      if(idx === count){
-        nodeToBeRemoved = current
-        previous.next = nodeToBeRemoved.next
-        nodeToBeRemoved.next = null
-      }
       count++;
+      current = current.next
     }
-    
+
+    nodeToBeRemoved = current
+    previous.next = nodeToBeRemoved.next
+    nodeToBeRemoved.next = null
+
     this.length--;
-    return nodeToBeRemoved;
+    return nodeToBeRemoved.val;
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
-    if(this.head === null) return 0;
+    if (this.head === null) return 0;
 
     let sum = 0;
     let current = this.head;
-    while(current !== null){
+    while (current !== null) {
       sum += current.val
       current = current.next
     }
 
-    return sum/this.length
+    return sum / this.length
+  }
+
+  reverseInPlace(){
+    if (this.length <2) return;
+
+    let left = null;
+    let right;
+    this.tail = this.head;
+
+    while (this.head.next != null){
+      
+      right = this.head.next;
+      this.head.next = left;
+      left = this.head;
+      this.head = right;
+    }
+    this.head.next = left;
+
   }
 
 }
